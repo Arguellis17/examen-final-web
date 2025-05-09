5a26b904f046f94c661205339cff2ed47008ed03
Arguellis17 <arguelloj866@gmail.com>
2025-05-09 06:35:08 -0500

    first commit

---
+++ src/main/java/plataforma_academica/plataforma_academica/ServletInitializer.java
@@ -0,0 +1,13 @@
+package plataforma_academica.plataforma_academica;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PlataformaAcademicaApplication.class);
+	}
+
+}
